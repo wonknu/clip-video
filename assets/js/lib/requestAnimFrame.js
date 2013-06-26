2306ae247f950c4e037edf95899749847ff1ea55
fabrice labbe <fabrice.labbe@adfab.fr>
2013-06-26 13:32:56 +0200

    deleted whole project that was moved to a branch

--- assets/js/lib/requestAnimFrame.js
+++
@@ -1,8 +0,0 @@
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// shim layer with setTimeout fallback
-window.requestAnimFrame = (function(callback) {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-      })();
--- jsonToAnim/js/app/starter.js
+++
@@ -1,5 +0,0 @@
-// jquery starter, do not put thing that doesn't need to be here!
-$(document).ready(function() {
-    window.saveFile = new SaveFile();
-    new AnimRecorder('../assets/videos/REVOLVER_MIX.mp4');
-});
